d464cb5af77acb2bb4e34b32d2d93044ce7eb549
Dennis <dennislinneman33@gmail.com>
2023-08-24 15:48:34 -0400

    pushing basic alg

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function bouncer(arr) {
+    var newArr = [];
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i]){
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr
+  }
+  
+  bouncer([7, "ate", "", false, 9]);
+
+
+
+  function chunkArrayInGroups(arr, size) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i+= size){
+      newArr.push(arr.slice(i, i+size))
+    }
+    return newArr;
+  }
+  
+  chunkArrayInGroups(["a", "b", "c", "d"], 2);
